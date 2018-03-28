9f96e352cea5edca4a0625647f706207dfe467c7
Lukasz Lenart <lukasz.lenart@gmail.com>
2018-03-28 11:45:31 +0200

    Adds initial install script

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+gem install prawn --version 1.3.0
+gem install addressable --version 2.4.0
+gem install prawn-svg --version 0.21.0
+gem install prawn-templates --version 0.0.3
+
+gem install prawn --version 2.1.0
+gem install prawn-svg --version 0.26.0
+gem install prawn-templates --version 0.0.4
+
+gem install asciidoctor-pdf --pre
+gem install rouge
+gem install pygments.rb
+gem install coderay
